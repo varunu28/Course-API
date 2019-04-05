258a3f5fad3ff5865a568dea8e24002b2023a553
varunu28 <varun.u28@gmail.com>
2019-04-04 19:29:00 -0700

    Finished CRUD operations for Course API

--- src/main/java/com/example/spring/topic/TopicService.java
+++ src/main/java/com/example/spring/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.example.spring.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
 			new Topic("javascript", "JavaScript", "JavaScript Description")
-		);
+			));
 	
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -20,5 +21,22 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); i++) {
+			if (topics.get(i).getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
